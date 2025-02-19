aa40f9809ed2c3af87b16e7bbbfc1d76dbc6e9aa
Luilu-star <luiotv2302@gmail.com>
2025-02-19 11:40:25 -0300

    Create exemplos_iniciais.py

---
+++ IED - 19.02/exemplos_iniciais.py
@@ -0,0 +1,21 @@
+mimde = 10
+
+def printa():
+    print(mimde)
+
+def ai():
+    if (mimde < 3):
+        print('ok')
+    else:
+        print('naoqueronaoquero')
+
+def loop():
+    for i in range(10):
+        print(i)
+
+#listas:
+
+lista_de_frutas = ['uva', 'banana', 'melancia', 'maracuja', 1, True, 3.141596238]
+
+print(lista_de_frutas.insert('murango'))
+
