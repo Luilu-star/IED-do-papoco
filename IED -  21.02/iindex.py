7fe11d5e9bed5de64322a38f36737cc8a78a66fa
Luilu-star <luiotv2302@gmail.com>
2025-02-21 22:48:49 -0300

    Uptades on IED

---
+++ IED -  21.02/iindex.py
@@ -0,0 +1,25 @@
+import lib
+
+numlist = []
+continua = 1
+
+while continua == 1:
+   numlist.append(int (input ('digite um número: ')))
+   print('Sua lista de números atual:', numlist)
+
+   continua = int(input('Deseja continuar? Sim[1] Não[2] '))
+
+print("Obrigado por contribuir com a lista!")
+#numlist.pop(indexdoelemento) Tira umm elemento desse específico index.
+#numlist.insert(index) Adiciona um elemento no index no index específico
+#numlist.append(elemento) Adiciona este elemento ao vetor
+
+#match variavel:  =  switch (variavel){
+#   case 1: xxx         case 1: xxx;}
+
+"""while True:
+   print('ok')
+   break
+
+if __name__ == 'main':
+    pass"""
